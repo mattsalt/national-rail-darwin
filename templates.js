b5f23f997daaaff6d1d6188de973b3caacf4e4f2
Josef Citrine <josefcitrine@gmail.com>
2017-01-11 21:08:06 +0000

    Added getNextDeparture, getArrival and location info

--- templates.js
+++ templates.js
@@ -48,7 +48,45 @@ var serviceDetailsTemplate = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.x
 +"   </soapenv:Body>"
 +"</soapenv:Envelope>";
 
+var nextDeparture = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:typ=\"http://thalesgroup.com/RTTI/2013-11-28/Token/types\" xmlns:ldb=\"http://thalesgroup.com/RTTI/2016-02-16/ldb/\">"
++"    <soap:Header>"
++"        <typ:AccessToken>"
++"            <typ:TokenValue>${TOKEN}</typ:TokenValue>"
++"        </typ:AccessToken>"
++"    </soap:Header>"
++"    <soap:Body>"
++"        <ldb:GetNextDeparturesRequest>"
++"            <ldb:crs>${STATION}</ldb:crs>"
++"            <ldb:filterList>"
++"                <ldb:crs>${DESTINATION}</ldb:crs>"
++"            </ldb:filterList>"
++"            <ldb:timeOffset>0</ldb:timeOffset>"
++"            <ldb:timeWindow>120</ldb:timeWindow>"
++"        </ldb:GetNextDeparturesRequest>"
++"    </soap:Body>"
++"</soap:Envelope>";
+
+var nextArrival = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:typ=\"http://thalesgroup.com/RTTI/2013-11-28/Token/types\" xmlns:ldb=\"http://thalesgroup.com/RTTI/2016-02-16/ldb/\">"
+    +"    <soap:Header>"
+    +"        <typ:AccessToken>"
+    +"            <typ:TokenValue>${TOKEN}</typ:TokenValue>"
+    +"        </typ:AccessToken>"
+    +"    </soap:Header>"
+    +"    <soap:Body>"
+    +"        <ldb:GetArrivalBoardRequest>"
+    +"            <ldb:crs>${STATION}</ldb:crs>"
+    +"            <ldb:filterList>"
+    +"                <ldb:crs>${DESTINATION}</ldb:crs>"
+    +"            </ldb:filterList>"
+    +"            <ldb:timeOffset>0</ldb:timeOffset>"
+    +"            <ldb:timeWindow>120</ldb:timeWindow>"
+    +"        </ldb:GetArrivalBoardRequest>"
+    +"    </soap:Body>"
+    +"</soap:Envelope>";
+
 
 module.exports.departureBoard = departureBoardTemplate;
 module.exports.arrivalsBoard = arrivalsBoardTemplate;
 module.exports.serviceDetails = serviceDetailsTemplate;
+module.exports.nextDeparture = nextDeparture;
+module.exports.nextArrival = nextArrival;
