5fcf0d6f0146f12a01872acdf0e17996fe7c1042
Matt <matthew.salt@gmail.com>
2017-02-15 18:55:18 +0000

    add remaining examples .xml files

--- templateStringTest.js
+++ templateStringTest.js
@@ -4,14 +4,13 @@ var fs = require('fs')
 // var parsedResult = parsers.parseServiceIdResponse(fileContent)
 // console.log(parsedResult)
 
-
 // var fileContent = fs.readFileSync('./exampleResponses/nextDepartureWithDetails.xml', 'UTF-8')
 // var parsedResult = parsers.parseNextDepartureWithDetailsResponse(fileContent)
 // console.log(parsedResult)
 
-var fileContent = fs.readFileSync('./exampleResponses/fastestDeparturesWithDetails.xml', 'UTF-8')
-var parsedResult = parsers.parseFastestDeparturesWithDetail(fileContent)
-console.log(parsedResult)
+// var fileContent = fs.readFileSync('./exampleResponses/fastestDeparturesWithDetails.xml', 'UTF-8')
+// var parsedResult = parsers.parseFastestDeparturesWithDetail(fileContent)
+// console.log(parsedResult)
 
 // var fileContent = fs.readFileSync('./exampleResponses/fastestDeparture.xml', 'UTF-8')
 // var parsedResult = parsers.parseFastestDeparture(fileContent)
@@ -25,7 +24,22 @@ console.log(parsedResult)
 // var parsedResult = parsers.parseArrivalsDepartureBoard(fileContent)
 // console.log(parsedResult)
 
-
 // var fileContent = fs.readFileSync('./exampleResponses/arrivalDepartureBoardWithDetails.xml', 'UTF-8')
 // var parsedResult = parsers.parseArrivalsDepartureBoardWithDetails(fileContent)
 // console.log(parsedResult)
+
+// var fileContent = fs.readFileSync('./exampleResponses/arrivalsBoard.xml', 'UTF-8')
+// var parsedResult = parsers.parseArrivalsBoardResponse(fileContent)
+// console.log(parsedResult)
+
+// var fileContent = fs.readFileSync('./exampleResponses/departureBoard.xml', 'UTF-8')
+// var parsedResult = parsers.parseDepartureBoardResponse(fileContent)
+// console.log(parsedResult)
+
+// var fileContent = fs.readFileSync('./exampleResponses/departureBoardWithDetails.xml', 'UTF-8')
+// var parsedResult = parsers.parseDepartureBoardWithDetailsResponse(fileContent)
+// console.log(parsedResult)
+
+var fileContent = fs.readFileSync('./exampleResponses/nextDeparture.xml', 'UTF-8')
+var parsedResult = parsers.parseNextDestinationResponse(fileContent)
+console.log(parsedResult)
