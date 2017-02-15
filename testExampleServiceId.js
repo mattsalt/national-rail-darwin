0b7a69adafda2194551e7500d8a3316204437aed
Matt <matthew.salt@gmail.com>
2017-02-15 18:57:46 +0000

    add test and examples xmls to npm ignore

--- testExampleServiceId.js
+++
@@ -1,9 +0,0 @@
-var parsers = require('./parsers.js')
-var fs = require('fs')
-var fileContent = fs.readFileSync('./exampleResponses/arrivalDepartureBoard.xml', 'UTF-8')
-
-console.log(fileContent)
-
-var parsedResult = parsers.parseServiceIdResponse(fileContent)
-
-console.log(parsedResult)
