ff37573e5e02356dd56e1f2efd5a664705934e57
Matt <matthew.salt@gmail.com>
2017-02-16 10:10:36 +0000

    Add test cases for arrivalsDeoartureBoard

--- test/test.js
+++ test/test.js
@@ -74,3 +74,87 @@ describe('Service Details parsing is correct', function () {
     assert.equal('On time', arr[0].et)
   })
 })
+
+describe('Arrival Departure Board with Details parsing is correct', function () {
+  var fileContent = fs.readFileSync('./exampleResponses/arrivalDepartureBoardWithDetails.xml', 'UTF-8')
+  var parsedResult = parsers.parseArrivalsDepartureBoardWithDetails(fileContent)
+
+  it('Arrival Departure Board sta = 21:34', function () {
+    assert.equal('21:34', parsedResult.trainServices[0].sta)
+  })
+  it('lArrival Departure Board eta = On time', function () {
+    assert.equal('On time', parsedResult.trainServices[0].eta)
+  })
+  it('Arrival Departure Boardstd = 21:34', function () {
+    assert.equal('21:34', parsedResult.trainServices[0].std)
+  })
+  it('Arrival Departure Board etd = On time', function () {
+    assert.equal('On time', parsedResult.trainServices[0].etd)
+  })
+  it('Arrival Departure Board platform = 1', function () {
+    assert.equal('1', parsedResult.trainServices[0].platform)
+  })
+  it('Arrival Departure Board operator = South West Trains', function () {
+    assert.equal('South West Trains', parsedResult.trainServices[0].operator)
+  })
+  it('Arrival Departure Board operatorCode = SW', function () {
+    assert.equal('SW', parsedResult.trainServices[0].operatorCode)
+  })
+  it('Arrival Departure Board length = 5', function () {
+    assert.equal('5', parsedResult.trainServices[0].length)
+  })
+  it('Arrival Departure BoardservicId = MrHNnCAHgnHGs5KtWtLkWw==', function () {
+    assert.equal('MrHNnCAHgnHGs5KtWtLkWw==', parsedResult.trainServices[0].serviceId)
+  })
+  it('Arrival Departure Board origin = WAT and LondonWaterloo', function () {
+    var origin = parsedResult.trainServices[0].origin
+    assert.equal('London Waterloo', origin.name)
+    assert.equal('WAT', origin.crs)
+  })
+  it('Arrival Departure Board origin = WAT and LondonWaterloo', function () {
+    var destination = parsedResult.trainServices[0].destination
+    assert.equal('London Waterloo', destination.name)
+    assert.equal('WAT', destination.crs)
+  })
+  it('PreviousCallingPoints array is correct size 14', function () {
+    var arr = parsedResult.trainServices[0].previousCallingPoints
+    assert.equal(14, arr.length)
+  })
+  it('Last previous calling point crs code = WAT', function () {
+    var arr = parsedResult.trainServices[0].previousCallingPoints
+    assert.equal('HOU', arr[13].crs)
+  })
+  it('Last previous calling point locationName = London Waterloo', function () {
+    var arr = parsedResult.trainServices[0].previousCallingPoints
+    assert.equal('Hounslow', arr[13].locationName)
+  })
+  it('Last previous calling point st = 09:45', function () {
+    var arr = parsedResult.trainServices[0].previousCallingPoints
+    assert.equal('21:31', arr[13].st)
+  })
+  it('Last previous calling point et = On time', function () {
+    var arr = parsedResult.trainServices[0].previousCallingPoints
+    assert.equal('On time', arr[13].et)
+  })
+  it('SubsequentCallingPoints array is correct size 14', function () {
+    var arr = parsedResult.trainServices[0].subsequentCallingPoints
+    assert.equal(12, arr.length)
+  })
+  it('First subsequent calling point crs code = WAT', function () {
+    var arr = parsedResult.trainServices[0].subsequentCallingPoints
+    assert.equal('SYL', arr[0].crs)
+  })
+  it('First subsequent calling point locationName = London Waterloo', function () {
+    var arr = parsedResult.trainServices[0].subsequentCallingPoints
+    assert.equal('Syon Lane', arr[0].locationName)
+  })
+  it('First subsequent calling point st = 09:45', function () {
+    var arr = parsedResult.trainServices[0].subsequentCallingPoints
+    assert.equal('21:36', arr[0].st)
+  })
+  it('First subsequent calling point et = On time', function () {
+    var arr = parsedResult.trainServices[0].subsequentCallingPoints
+    assert.equal('On time', arr[0].et)
+  })
+})
+
